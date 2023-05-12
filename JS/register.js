e2ba159d02a7fcca34aa12f224248dc9c8e31db8
McAfees <133426748+McAfees@users.noreply.github.com>
2023-05-12 16:57:44 -0600

    Add files via upload

---
+++ JS/register.js
@@ -0,0 +1,10 @@
+//Iniciar sesión y registro
+const loginsec=document.querySelector('.login-section')
+const loginlink=document.querySelector('.login-link')
+const registerlink=document.querySelector('.register-link')
+registerlink.addEventListener('click',()=>{
+    loginsec.classList.add('active')
+})
+loginlink.addEventListener('click',()=>{
+    loginsec.classList.remove('active')
+})
